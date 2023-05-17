d1f725dfd7327cde050c041dc444555ec6a8c305
zainalabidin10 <mz4191384@gmail.com>
2023-05-17 15:12:37 +0700

    view

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DataKendaraanController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Example Routes
+Route::view('/', 'landing');
+Route::match(['get', 'post'], '/dashboard', function(){
+    return view('dashboard');
 });
+Route::view('/pages/slick', 'pages.slick');
+Route::view('/pages/datatables', 'pages.datatables');
+Route::view('/pages/blank', 'pages.blank');
+Route::view('/pages/datakendaraan','pages.datakendaraan');
+Route::view('/pages/riwayattransaksi','pages.riwayattransaksi');
+
+Route::get('pages/datakendaraan', [DataKendaraanController::class, 'index'])->name('datakendaraan.index');
+Route::get('/pages/datakendaraan/create', [DataKendaraanController::class, 'create']);
+Route::post('/datakendaraan', [DataKendaraanController::class, 'store'])->name('datakendaraan.store');
